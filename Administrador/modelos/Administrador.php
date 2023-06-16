c36611ffa35eac74119124a001cb907aafed00b7
FelipeLeal34 <andresfelipelealculma34@gmail.com>
2023-06-16 18:49:15 -0500

    agregar admin & prueba

--- Administrador/modelos/Administrador.php
+++ Administrador/modelos/Administrador.php
@@ -1,8 +1,32 @@
 <?php
 
-    class Administrador{
+
+    include_once("../../Conexion.php");
+
+    class Administrador extends Conexion{
+
+        public function __construct(){
+            $this->db = parent::__construct();
+        }
+
+        public function agregarad($nombread,$apellidoad,$usuarioad,$passwordad,$perfilad,$estadoad){
+
+            $statement = $this->db->prepare("INSERT INTO usuarios(Nombreusu,Apellidousu,Usuario,Passwordusu,Perfil,Estado)VALUES(:nombread,:apellidoad,:usuarioad,:passwordad,:perfilad,:estadoad)");
 
 
+            $statement->bindParam(":nombread",$nombread);
+            $statement->bindParam(":apellidoad",$apellidoad);
+            $statement->bindParam(":usuarioad",$usuarioad);
+            $statement->bindParam(":passwordad",$passwordad);
+            $statement->bindParam(":perfilad",$perfilad);
+            $statement->bindParam(":estadoad",$estadoad);
+
+            if ($statement->execute()) {
+                echo "usuario registrado";
+            } else {
+                echo "usuario no registrado";
+            }
+        }
 
     }
 ?>
---
+++ conexion.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Conexion{
+
+	protected $db;
+	private $drive = "mysql";
+	private $host = "localhost";
+	private $dbname = "notas2023";
+	private $usuario = "root";
+	private $contra = "";
+
+
+	public function __construct(){
+		try {
+			$db = new PDO("{$this->drive}:host={$this->host};dbname={$this->dbname}", $this->usuario,$this->contra);
+
+
+
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+
+			//return $db;
+
+
+			echo "Conexion realizada";
+
+		} catch (PDOException $e) {
+			echo "Conexion no exitosa: " . $e->getMessage();
+		}
+	}
+
+}
+
+ ?>
